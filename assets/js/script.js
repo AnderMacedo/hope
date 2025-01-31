bf938796161ea0a195da1cdb6dd40f86254715dd
AnderMacedo <u20201c179@upc.edu.pe>
2025-01-31 11:37:24 -0500

    fix: bug del sidebar y detalles de productos arreglados

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,5 @@
 // /assets/js/script.js
 
-document.addEventListener("DOMContentLoaded", function() {
-    // Crear una solicitud de HTTP para cargar el archivo navbar.html
-    fetch('/components/navbar.html')
-        .then(response => response.text())
-        .then(data => {
-            // Insertar el contenido del navbar en el contenedor con id="navbar"
-            document.getElementById('navbar').innerHTML = data;
-        })
-        .catch(error => console.error('Error al cargar el navbar:', error));
-});
-
 // script.js
 $(document).ready(function() {
     // Función global para actualizar el contador del carrito
